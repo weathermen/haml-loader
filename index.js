cf511af5d89cc56e3a43140167c2cc148fe33e20
Tom Scott <tscott@weblinc.com>
2018-12-28 11:29:52 -0500

    Fix hamljs renderer

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 module.exports = function (source) {
   var loaderUtils, haml, query, result
+
   this.cacheable && this.cacheable(true)
+
   loaderUtils = require("loader-utils")
   haml = require("hamljs")
   query = loaderUtils.parseQuery(this.query)
+
   try {
-    result = haml.compile(source, query)
+    result = haml.render(source)
   } catch (e) {
     this.emitError('HAML:' + e)
     throw e
   }
+
   return "module.exports = " + JSON.stringify(result) + ";"
 }
